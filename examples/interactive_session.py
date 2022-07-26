2708d100a818f13e3e3bc18d746962d3fc10366f
AndriyMulyar <andriy.mulyar@gmail.com>
2022-07-25 23:33:54 -0400

    Indices can be successfully created

--- examples/interactive_session.py
+++ examples/interactive_session.py
@@ -1,46 +1,24 @@
 from nomic import AtlasClient
+import numpy as np
 
 atlas = AtlasClient()
 
+num_embeddings = 10000
+embeddings = np.random.rand(num_embeddings, 256)
+data = [{'id': i, 'text': str(uuid.uuid4())} for i in range(len(embeddings))]
 
-print(atlas._get_user())
+project_name = 'test'
+index_name = 'test'
 
-# projects = atlas.get_projects()
-#
-# print("Available projects:", projects)
-# if not projects:
-#     exit(0)
-#
-# project = projects[0]
-#
-# #Get currently tagged documents
-#
-# tagged_documents = atlas.get_documents_by_tags(project=project)
-#
-# if tagged_documents:
-#     print(tagged_documents)
-#     tags = list(tagged_documents.keys())
-#     tag = tags[0]
-#     ids_to_documents = atlas.get_documents_by_ids(project=project, ids=tagged_documents[tag])
-#     print(f"Num documents tagged with tag `{tag}`: ", len(ids_to_documents))
-#     print(list(ids_to_documents.values())[0])
-# else:
-#     print("No currently tagged documents")
-#
-# # add a new tag to all of the documents
-# print("Adding the tag `my_custom_tag` for all documents.")
-# all_documents = atlas.get_documents(project=project)
-# atlas.tag_documents(project=project, ids=[doc['_id'] for doc in all_documents], tags=['my_custom_tag'])
-#
-# newly_tagged_documents = atlas.get_documents_by_tags(project=project)
-# print("All document tags:", newly_tagged_documents)
-#
-#
-#
-# #remove the tag
-# atlas.untag_documents(project=project, ids=[doc['_id'] for doc in all_documents], tags=['my_custom_tag'])
-# print("Removed the tag `my_custom_tag` for all documents.")
-#
-#
-#
-#
+atlas.create_project(project_name=project_name,
+                     description=project_name,
+                     unique_id_field='id',
+                     modality='embedding')
+
+print("Adding Data")
+atlas.add_embeddings(project_name=project_name,
+                     embeddings=embeddings,
+                     data=data)
+
+print("Organizing Data")
+atlas.create_index(project_name=project_name, index_name=index_name)
--- nomic/atlas.py
+++ nomic/atlas.py
@@ -4,9 +4,11 @@ The Official Nomic Python Client for Atlas
 This class allows for programmatic interactions with Atlas. Initialize AtlasClient in any Python context such as a script
 or in a Jupyter Notebook to organize your data.
 """
+import concurrent.futures
 import json
 import os
 from typing import Dict, List
+
 import numpy as np
 import requests
 
@@ -41,17 +43,20 @@ class AtlasClient:
                 "Could not find an authorization token. Run `nomic login` to authorize this client with the Nomic API."
             )
 
-    def _get_user(self):
+    def _get_current_user(self):
         response = requests.get(
             self.atlas_api_path + "/v1/user",
             headers=self.header,
         )
         if not response.status_code == 200:
-            print("Your authorization token is no longer valid.")
-            exit()
+            raise ValueError("Your authorization token is no longer valid.")
+
         return response.json()
 
-    def create_project(self, project_name: str, description: str, unique_id_field: str, modality: str, is_public: bool = True):
+
+    def create_project(
+        self, project_name: str, description: str, unique_id_field: str, modality: str, is_public: bool = True
+    ):
         '''
         Creates an Atlas project. Atlas projects store data (text, embeddings, etc) that you can organize by building indices.
 
@@ -67,14 +72,7 @@ class AtlasClient:
 
         '''
 
-        user = self._get_user()
-        if len(user['organizations']) > 1:
-            raise NotImplementedError("This client does not support users in more than one organization yet.")
-
-        organization_id = user['organizations'][0]['organization_id']
-
-        if modality not in ['text', 'embedding']:
-            raise ValueError("Atlas currently only supports 'text' and 'embedding' projects.")
+        organization_id = self._get_current_users_main_organization()
 
         response = requests.post(
             self.atlas_api_path + "/v1/project/create",
@@ -85,45 +83,166 @@ class AtlasClient:
                 'description': description,
                 'unique_id_field': unique_id_field,
                 'modality': modality,
-                'is_public': is_public
+                'is_public': is_public,
             },
         )
         if response.status_code != 201:
             raise Exception(f"Failed to create project: {response.json()}")
         return True
 
+    def _get_current_users_main_organization(self):
+        '''
+        Retrieves the ID of the current users default organization.
 
-    def add_embeddings(self,
-                       project: str,
-                       embeddings: np.array,
-                       data: List[Dict]
-                       ):
+        Returns:
+            The ID of the current users default organization.
+
+        '''
+
+        user = self._get_current_user()
+        for organization in user['organizations']:
+            if organization['user_id'] == user['sub'] and organization['access_role'] == 'OWNER':
+                return organization['organization_id']
+
+
+    def _get_project_by_name(self, project_name):
+        '''
+        Retrieves a project that a user owns by name.
+        Args:
+            project_name: the project name
+
+        Returns:
+
+        '''
+
+        organization_id = self._get_current_users_main_organization()
+
+        organization = requests.get(
+            self.atlas_api_path + f"/v1/organization/{organization_id}",
+            headers=self.header,
+        ).json()
+
+        target_project = None
+        for candidate in organization['projects']:
+            if candidate['project_name'] == project_name:
+                target_project = candidate
+                break
+
+        if target_project is None:
+            raise ValueError(f"Could not find project `{project_name}`")
+
+        return target_project
+
+
+
+
+    def add_embeddings(self, project_name: str, embeddings: np.array, data: List[Dict]):
         '''
         Adds embeddings to an embedding project. Pair each embedding with meta-data to explore your embeddings.
 
         Args:
-            project: The name of the project.
+            project_name: The name of the project.
             embeddings: An [N,d] numpy array containing the batch of N embeddings to add.
             data: An [N,] element list of dictionaries containing metadata for each embedding.
 
         Returns:
             True on success.
         '''
-        pass
+
+        target_project_id = self._get_project_by_name(project_name=project_name)['id']
+
+        shard_size = 1000
+
+        def send_request(i):
+            data_shard = data[i : i + shard_size]
+            embedding_shard = embeddings[i : i + shard_size, :].tolist()
+            response = requests.post(
+                self.atlas_api_path + "/v1/project/data/add/embedding/initial",
+                headers=self.header,
+                json={'project_id': target_project_id, 'embeddings': embedding_shard, 'data': data_shard},
+            )
+            return response
+
+        failed = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = {executor.submit(send_request, i): i for i in range(0, len(data), shard_size)}
+            for future in concurrent.futures.as_completed(futures):
+                response = future.result()
+                if response.status_code != 200:
+                    failed.append(futures[future])
+
+        if failed:
+            raise ValueError("Failed to upload a subset of datums")
+
+        return True
+
+    def create_index(self, project_name, index_name):
+
+        project = self._get_project_by_name(project_name=project_name)
+
+        index_build_template = {
+            'project_id': project['id'],
+            'index_name': index_name,
+            'indexed_field': None,
+            'atomizer_strategies': None,
+            'model': None,
+            'model_hyperparameters': None,
+            'nearest_neighbor_index': 'HNSWIndex',
+            'nearest_neighbor_index_hyperparameters': json.dumps({'space': 'l2', 'ef_construction': 100, 'M': 16}),
+            'projection': 'UMAPProjection',
+            'projection_hyperparameters': json.dumps(
+                {'n_neighbors': 15, 'min_dist': 3e-2, 'force_approximation_algorithm': True}
+            ),
+        }
+
+        if project['modality'] == 'embedding':
+            response = requests.post(
+                self.atlas_api_path + "/v1/project/index/create",
+                headers=self.header,
+                json=index_build_template,
+            )
+            job_id = response.json()['job_id']
+
+        elif project['modality'] == 'text':
+            raise NotImplementedError("Building indices for text based projects is not yet implemented in this client.")
+
+        job = requests.get(
+            self.atlas_api_path + f"/v1/project/index/job/{job_id}",
+            headers=self.header,
+        ).json()
+
+        index_id = job['index_id']
+
+        project = requests.get(
+            self.atlas_api_path + f"/v1/project/{project['id']}",
+            headers=self.header,
+        ).json()
+
+        projection_id = None
+
+        for index in project['atlas_indices']:
+            if index['id'] == index_id:
+                projection_id = index['projections'][0]['id']
+                break
+
+        if not projection_id:
+            print("Could not find a projection being built for this index.")
+        else:
+            print(f"https://atlas.nomic.ai/scatter/{project['id']}/{projection_id}")
 
 
-    def add_text(self, project: str, data: List[Dict]):
+    def add_text(self, project_name: str, data: List[Dict]):
         '''
         Adds text to an Atlas text project. Each text datum consists of a keyed dictionary.
         Args:
-            project: The name of the project.
+            project_name: The name of the project.
             data: A [N,] element list of dictionaries containing your datums.
 
         Returns:
             True if success.
 
         '''
-        pass
+        raise NotImplementedError("Building indices for text based projects is not yet implemented in this client.")
 
     def map_embeddings(self, embeddings: np.array, data: List[Dict]):
         '''
@@ -136,6 +255,9 @@ class AtlasClient:
         Returns:
 
         '''
+
+
+
         pass
 
     # def get_projects(self) -> List:
