8de63853b28262ac3a22d6fb83c1d17dda391dff
AndriyMulyar <andriy.mulyar@gmail.com>
2022-08-03 17:01:35 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 description = 'The offical Nomic python client.'
 setup(
     name='nomic',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
