307bd701fa4e97621ce1f36032c534bc080ee054
AndriyMulyar <andriy.mulyar@gmail.com>
2022-08-14 17:56:13 -0400

    Version 1.0.11: Improved documentation and logging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 description = 'The offical Nomic python client.'
 setup(
     name='nomic',
-    version='1.0.10',
+    version='1.0.11',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
