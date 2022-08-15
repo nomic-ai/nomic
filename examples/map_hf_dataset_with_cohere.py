92cfce09ece754db4e50bef81a1627c748e8df8b
AndriyMulyar <andriy.mulyar@gmail.com>
2022-08-15 11:23:20 -0400

    Improved cohere map preview

--- examples/map_hf_dataset_with_cohere.py
+++ examples/map_hf_dataset_with_cohere.py
@@ -21,7 +21,7 @@ print(f"Embedding {len(documents)} documents with Cohere API")
 embeddings = embedder.embed(texts=[document['user'] for document in documents],
                             model='small',
                             num_workers=10,
-                            shard_size=1000)
+                            shard_size=1000,)
 
 if len(embeddings) != len(documents):
     raise Exception("Embedding job failed")
@@ -32,8 +32,8 @@ response = atlas.map_embeddings(embeddings=np.array(embeddings),
                                 id_field='id',
                                 colorable_fields=['sentiment'],
                                 is_public=True,
+                                map_name='Sentiment 140',
+                                map_description='A 10,000 point sample of the huggingface sentiment140 dataset embedded with the co:here small model.',
                                 organization_name=None, #defaults to your current user.
                                 num_workers=20)
-
-print("Embedding Map:")
 print(response)
