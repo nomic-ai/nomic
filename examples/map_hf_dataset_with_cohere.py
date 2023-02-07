4ac605e78276c6cc8c870ac3f92d2f0b4bf9b1bd
AndriyMulyar <andriy.mulyar@gmail.com>
2023-02-07 17:21:10 -0500

    Updated all examples

--- examples/map_hf_dataset_with_cohere.py
+++ examples/map_hf_dataset_with_cohere.py
@@ -1,9 +1,8 @@
-from nomic import AtlasClient
+from nomic import atlas
 from nomic import CohereEmbedder
 import numpy as np
 from datasets import load_dataset
 
-atlas = AtlasClient()
 cohere_api_key = ''
 
 dataset = load_dataset("sentiment140")['train']
@@ -16,9 +15,7 @@ embedder = CohereEmbedder(cohere_api_key=cohere_api_key)
 
 print(f"Embedding {len(documents)} documents with Cohere API")
 embeddings = embedder.embed(texts=[document['user'] for document in documents],
-                            model='small',
-                            num_workers=10,
-                            shard_size=1000,)
+                            model='small')
 
 if len(embeddings) != len(documents):
     raise Exception("Embedding job failed")
@@ -27,9 +24,7 @@ print("Embedding job complete.")
 response = atlas.map_embeddings(embeddings=np.array(embeddings),
                                 data=documents,
                                 colorable_fields=['sentiment'],
-                                is_public=True,
                                 map_name='Sentiment 140',
-                                map_description='A 10,000 point sample of the huggingface sentiment140 dataset embedded with the co:here small model.',
-                                organization_name=None, #defaults to your current user.
-                                num_workers=20)
+                                map_description='A 10,000 point sample of the huggingface sentiment140 dataset embedded with cohere',
+                                )
 print(response)
