5b1cc02eceb624626e2fad14c00ab1e154eabe1c
AndriyMulyar <andriy.mulyar@gmail.com>
2023-02-08 17:22:10 -0500

    Interactive session

--- examples/interactive_session.py
+++ examples/interactive_session.py
@@ -1,26 +1,23 @@
-from nomic import AtlasClient
+from nomic import atlas, AtlasProject
+import nomic
 import numpy as np
+from datasets import load_dataset
+# dataset = load_dataset('ag_news')['train']
+#
+# np.random.seed(0)
+# max_documents = 25000
+# subset_idxs = np.random.randint(len(dataset), size=max_documents).tolist()
+# documents = [dataset[i] for i in subset_idxs]
+#
+# project = atlas.map_text(data=documents,
+#                           indexed_field='text',
+#                           map_name='News Dataset 25k',
+#                           colorable_fields=['label'],
+#                           map_description='News Dataset 25k'
+#                           )
 
-atlas = AtlasClient()
+map = AtlasProject(name='News Dataset 25k')
+print(map.total_datums)
+print(map.get_tags())
 
-num_embeddings = 10000
-embeddings = np.random.rand(num_embeddings, 256)
-data = [{'id': i} for i in range(len(embeddings))]
 
-project_name = 'My Project'
-index_name = 'My Index'
-
-project_id = atlas.create_project(project_name=project_name,
-                     description=project_name,
-                     modality='embedding')
-
-print(f"Adding embeddings to project: {project_id}")
-atlas.add_embeddings(project_id=project_id,
-                     embeddings=embeddings,
-                     data=data)
-
-print(f"Organizing embeddings in project: {project_id}")
-response = atlas.create_index(project_id=project_id, index_name=index_name)
-print(response)
-
-print(atlas._get_index_job(job_id=response.job_id))
