9936da4caf415bce4374d8493313815345028f81
AndriyMulyar <andriy.mulyar@gmail.com>
2023-02-15 12:07:01 -0500

    reset interaction session file

--- examples/interactive_session.py
+++ examples/interactive_session.py
@@ -1,30 +1,23 @@
 from nomic import atlas, AtlasProject
 import numpy as np
 from datasets import load_dataset
-# dataset = load_dataset('ag_news')['train']
-#
-# np.random.seed(0)
-# max_documents = 25000
-# subset_idxs = np.random.randint(len(dataset), size=max_documents).tolist()
-# documents = [dataset[i] for i in subset_idxs]
-#
-# project = atlas.map_text(data=documents,
-#                           indexed_field='text',
-#                           map_name='News Dataset 25k',
-#                           colorable_fields=['label'],
-#                           map_description='News Dataset 25k'
-#                           )
+dataset = load_dataset('ag_news')['train']
 
-project = AtlasProject(name='News Dataset 25k')
+np.random.seed(0)
+max_documents = 25000
+subset_idxs = np.random.randint(len(dataset), size=max_documents).tolist()
+documents = [dataset[i] for i in subset_idxs]
 
-answer, evidence = project._ask_question(question='Where is New York City?',
-                                         cohere_api_key='',
-                                         evidence_field='text',)
+project = atlas.map_text(data=documents,
+                          indexed_field='text',
+                          map_name='News Dataset 25k',
+                          colorable_fields=['label'],
+                          map_description='News Dataset 25k'
+                          )
 
-print(answer)
-print(evidence)
 
-# map = project.get_map(name='News Dataset 25k')
-# print(map.map_link)
-# print(project.total_datums)
+
+map = project.get_map(name='News Dataset 25k')
+print(map.map_link)
+print(project.total_datums)
 
