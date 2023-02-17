7b81b4f24aaee5a12f3385fec78909db1d1fcb80
AndriyMulyar <andriy.mulyar@gmail.com>
2023-02-17 15:43:07 -0500

    remove add tags from old pr

--- nomic/project.py
+++ nomic/project.py
@@ -1491,27 +1491,3 @@ class AtlasProject(AtlasClass):
                     label_to_datums[label] = []
                 label_to_datums[label].append(item['datum_id'])
         return label_to_datums
-
-    def add_tags(self) -> Dict[str, str]:
-        '''
-        Retrieves back all tags made in the web browser for a specific project and map.
-
-        Returns:
-            A dictionary mapping datum ids to tags.
-        '''
-        # now get the tags
-        datums_and_tags = requests.post(
-            self.atlas_api_path + '/v1/project/tag/read/all_by_datum',
-            headers=self.header,
-            json={
-                'project_id': self.id,
-            },
-        ).json()['results']
-
-        label_to_datums = {}
-        for item in datums_and_tags:
-            for label in item['labels']:
-                if label not in label_to_datums:
-                    label_to_datums[label] = []
-                label_to_datums[label].append(item['datum_id'])
-        return label_to_datums
