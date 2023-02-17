615dfa29163f3909b71a220e5fc21d13229248bc
AndriyMulyar <andriy.mulyar@gmail.com>
2023-02-17 15:37:02 -0500

    Isnan check

--- nomic/project.py
+++ nomic/project.py
@@ -241,10 +241,11 @@ class AtlasClass(object):
                             msg = 'Datum {} had an empty string for key: {}'.format(datum, key)
                             raise ValueError(msg)
                 import math
-                if isinstance(datum[key], float) and math.nan(datum[key]):
-                    raise Exception(
-                        f"Metadata sent to Atlas must be a flat dictionary. Values must be strings, floats or ints. Key `{key}` of datum {str(datum)} is in violation."
-                    )
+                if isinstance(datum[key], float):
+                    if math.isnan(datum[key]):
+                        raise Exception(
+                            f"Metadata sent to Atlas must be a flat dictionary. Values must be strings, floats or ints. Key `{key}` of datum {str(datum)} is in violation."
+                        )
 
                 if not isinstance(datum[key], (str, float, int)):
                     raise Exception(
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 description = 'The offical Nomic python client.'
 setup(
     name='nomic',
-    version='1.0.35',
+    version='1.0.37',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
