53031046bb7c5f6745a15d4a0801510f5a7758cd
Ben Schmidt <bmschmidt@gmail.com>
2023-02-17 16:42:34 -0500

    remove double call to latest_project_state

--- nomic/project.py
+++ nomic/project.py
@@ -806,7 +806,6 @@ class AtlasProject(AtlasClass):
         Returns:
             True if project is unlocked for data additions, false otherwise.
         '''
-        self._latest_project_state()
         return not self.is_locked
 
 
