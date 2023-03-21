19b8bf42fe3fa76f289eae5bdd3ec54de79d33fe
Benjamin Schmidt <bmschmidt@gmail.com>
2023-03-21 13:11:22 -0400

    Update setup.py
    Call this a minor version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 description = 'The offical Nomic python client.'
 setup(
     name='nomic',
-    version='1.0.52',
+    version='1.1.0',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
