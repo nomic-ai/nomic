6a42a263fc03c04a0125f8b508de15ba1786b568
bstadt <brandonduderstadt@gmail.com>
2023-03-29 13:50:51 -0400

    added download and easier gpu loading interface

--- nomic/gpt4all.py
+++ nomic/gpt4all.py
@@ -1,36 +1,64 @@
-import subprocess
-import time
+import os
 import sys
-import random    
+import time
+import torch
+import random
+import requests
+import subprocess
+from tqdm import tqdm
 from pathlib import Path
-# import eliza
+from loguru import logger
 
-prompts = [
-    'Write me a letter from the perspective of a cat',
-    'Write me a short poem',
-    'Tell me how to hard boil an egg',
-    'Come up with the vacation destinations.'
-]
+class GPT4AllGPU():
+    def __init__(self, alpaca_path=None):
+        if alpaca_path is None:
+            raise ValueError('Please pass a path to your alpaca model.')
 
-def download_executable():
-    pass
+        self.model_path = alpaca_path
+        self.tokenizer_path = alpaca_path
+        self.lora_path = 'nomic-ai/vicuna-lora-multi-turn_epoch_2'
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_path,
+                                                          device_map="auto",
+                                                          torch_dtype=torch.float16)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
+        added_tokens = tokenizer.add_special_tokens({"bos_token": "<s>", "eos_token": "</s>", "pad_token": "<pad>"})
 
-def download_model(modelname):
-    pass
+        if added_tokens > 0:
+            model.resize_token_embeddings(len(tokenizer))
 
-executable_path = Path("~/.nomic/gpt4all").expanduser()
-model_path = Path("~/.nomic/gpt4all-lora-quantized.bin").expanduser()
+        self.model = PeftModelForCausalLM.from_pretrained(self.model,
+                                                          self.lora_path,
+                                                          device_map="auto",
+                                                          torch_dtype=torch.float16)
+        self.model.to(dtype=torch.float16)
+        print(f"Mem needed: {self.model.get_memory_footprint() / 1024 / 1024 / 1024:.2f} GB")
+
 
 class GPT4All():
-    def __init__(self, download=True):
-        if not (executable_path.exists() and model_path.exists()):
-            if not download:
-                raise Exception("You need to download the executable and model first.")
-            else:
-                raise     
-        self.bot = subprocess.Popen([executable_path, '--model', model_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    def __init__(self, force_download=False):
+        self.executable_path = Path("~/.nomic/gpt4all").expanduser()
+        self.model_path = Path("~/.nomic/gpt4all-lora-quantized.bin").expanduser()
+        if force_download or not (self.executable_path.exists() or self.model_path.exists()):
+            logger.info('Downloading model...')
+            self._download()
+        self.bot = subprocess.Popen([executable_path, '--model', model_path],
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE)
         # queue up the prompt.
         self.parse_to_prompt()
+
+    def _download(self):
+        response = requests.get('https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/gpt4all-lora-quantized.bin',
+                                stream=True)
+        if response.status_code == 200:
+            os.makedirs(self.executable_path, exist_ok=True)
+            total_size = int(response.headers.get('content-length', 0))
+            with open(self.model_path, "wb") as f:
+                for chunk in tqdm(response.iter_content(chunk_size=8192), total=total_size // 8192, unit='KB'):
+                    f.write(chunk)
+            print(f"File downloaded successfully to {target_path}")
+        else:
+            print(f"Failed to download the file. Status code: {response.status_code}")
 
     def parse_to_prompt(self, write_to_stdout = True):
         bot_says = ['']
@@ -62,17 +90,6 @@ class GPT4All():
         bot.stdin.flush()
         return self.parse_to_prompt()
     
-    def therapy(self):
-        session = eliza.Eliza()
-        session.load('doctor.txt')
-        bot = self.bot
-        eliza_says = session.initial()
-        print("\n\nELIZA: " + eliza_says)
-        while True:
-            bot_says = self.prompt(eliza_says)
-            eliza_says = session.respond(bot_says)
-            print("\n\nELIZA: " + eliza_says)
-        
+
 if __name__ == '__main__':
-    session = BotSession()
-    session.therapy()
+    model = GPT4All(force_download=True)
