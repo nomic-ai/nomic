756bdc6b05507189cdbc713ff70049a248be47d8
Aaron Miller <apage43@ninjawhale.com>
2023-06-06 12:04:11 -0700

    override path is *optional*

--- nomic/project.py
+++ nomic/project.py
@@ -61,7 +61,7 @@ class AtlasClass(object):
 
         self.atlas_api_path = f"https://{api_hostname}"
         self.web_path = f"https://{web_hostname}"
-        override_api_path = os.environ['ATLAS_API_PATH']
+        override_api_path = os.environ.get('ATLAS_API_PATH')
         if override_api_path:
             self.atlas_api_path = override_api_path
 
