8b8b46ad4020af80e5429692f0e87a873c091f3b
Andriy Mulyar <andriy.mulyar@gmail.com>
2023-06-26 16:33:29 -0400

    reverts map text example

--- examples/map_text.py
+++ examples/map_text.py
@@ -1,25 +1,19 @@
-# from nomic import atlas
-# import numpy as np
-# from datasets import load_dataset
-#
-# dataset = load_dataset('ag_news')['train']
-#
-# max_documents = 10000
-# subset_idxs = np.random.randint(len(dataset), size=max_documents).tolist()
-# documents = [dataset[i] for i in subset_idxs]
-#
-# project = atlas.map_text(data=documents,
-#                           indexed_field='text',
-#                           name='News 10k Example For Data Access',
-#                           colorable_fields=['label'],
-#                           description='News 10k Example from the ag_news dataset hosted on huggingface.'
-#                           )
+from nomic import atlas
+import numpy as np
+from datasets import load_dataset
 
-from nomic import AtlasProject
+dataset = load_dataset('ag_news')['train']
 
-project = AtlasProject(name='News 10k Example For Data Access')
-map = project.maps[0]
-# print(map.topics.df)
-print(map.topics.df)
+max_documents = 10000
+subset_idxs = np.random.randint(len(dataset), size=max_documents).tolist()
+documents = [dataset[i] for i in subset_idxs]
+
+project = atlas.map_text(data=documents,
+                          indexed_field='text',
+                          name='News 10k Example',
+                          colorable_fields=['label'],
+                          description='News 10k Example from the ag_news dataset hosted on huggingface.'
+                          )
+print(project.maps)
 
 
