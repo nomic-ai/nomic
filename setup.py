83361c2236cb77ec0262b5a9267bdf73080c7203
Andriy Mulyar <andriy.mulyar@gmail.com>
2023-06-27 12:32:07 -0400

    Latest state

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
     
 setup(
     name='nomic',
-    version='1.1.14',
+    version='2.0.0',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
