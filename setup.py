f52916086a77e4a8b84afbe9a3b96206ac133342
Andriy Mulyar <andriy.mulyar@gmail.com>
2023-06-29 10:08:50 -0400

    setup.py erro

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='2.0.2',
     url='https://github.com/nomic-ai/nomic',
     description=description,
-    long_description=long_description,
+    long_description=description,
     packages=find_packages(),
     author_email="support@nomic.ai",
     author="nomic.ai",
