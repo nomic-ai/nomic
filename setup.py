f294b0c797b68e3f53d49aef130296e5796baafc
Richard Guo <richardg7890@gmail.com>
2023-09-26 14:58:28 -0400

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description = 'The official Nomic python client.'
     
 setup(
     name='nomic',
-    version='2.0.10',
+    version='2.0.11',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
