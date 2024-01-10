479532c0a40620c42c9e6a5306e3eecf7273dfd0
Andriy Mulyar <andriy.mulyar@gmail.com>
2024-01-09 22:45:47 -0500

    v3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description = 'The official Nomic python client.'
     
 setup(
     name='nomic',
-    version='2.0.16',
+    version='3.0.0',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
