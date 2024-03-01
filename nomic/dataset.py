189856cfdab18c35710aab8abddfcf586923ca27
Richard Guo <richardg7890@gmail.com>
2024-03-01 15:37:38 -0500

    duplicate detection for embeddings

--- nomic/dataset.py
+++ nomic/dataset.py
@@ -1096,9 +1096,6 @@ class AtlasDataset(AtlasClass):
         else:
             embedding_model = NomicEmbedOptions()
 
-        if self.modality == 'embedding':
-            duplicate_detection.tag_duplicates = False
-
         # for large datasets, alter the default projection configurations.
         if self.total_datums >= 1_000_000:
             if (
@@ -1124,8 +1121,6 @@ class AtlasDataset(AtlasClass):
             raise ValueError("`rho` must be in [0,1]")
 
         if self.modality == 'embedding':
-            if duplicate_detection.tag_duplicates:
-                raise ValueError("Cannot tag duplicates in an embedding dataset.")
             if topic_model.community_description_target_field is None:
                 logger.warning(
                     "You did not specify the `topic_label_field` option in your topic_model, your dataset will not contain auto-labeled topics."
@@ -1157,6 +1152,12 @@ class AtlasDataset(AtlasClass):
                         'community_description_target_field': topic_model.community_description_target_field,
                         'cluster_method': topic_model.cluster_method,
                         'enforce_topic_hierarchy': topic_model.enforce_topic_hierarchy,
+                    }
+                ),
+                'duplicate_detection_hyperparameters': json.dumps(
+                    {
+                        'tag_duplicates': duplicate_detection.tag_duplicates,
+                        'duplicate_cutoff': duplicate_detection.duplicate_cutoff,
                     }
                 ),
             }
