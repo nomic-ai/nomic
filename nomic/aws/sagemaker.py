96d50009874790018cbed8904994d4f90ea60692
Richard Guo <richardg7890@gmail.com>
2024-03-22 14:30:24 -0400

    no longer need any triton support

--- nomic/aws/sagemaker.py
+++ nomic/aws/sagemaker.py
@@ -3,15 +3,13 @@
 # it currently lives here to separate out dependencies.
 
 import hashlib
+import json
 import logging
 from enum import Enum
-from pathlib import Path
-from typing import List, Optional
+from typing import List
 
 import boto3
 import numpy as np
-from tokenizers import Tokenizer
-from tritonclient.http import InferenceServerClient, InferInput, InferRequestedOutput
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -54,86 +52,6 @@ class NomicTextEmbeddingModel(Enum):
         return text_embedding_model_info[self.value]["pad_id"]
 
 
-def null_empty_placeholder(text: Optional[str]) -> str:
-    """
-    If text is null or empty replace with a placeholder nonsense string.
-    """
-    if text is None:
-        return NULL_PLACEHOLDER
-    if text.strip() == "":
-        return EMPTY_PLACEHOLDER
-    return text
-
-
-def load_tokenizer(model: NomicTextEmbeddingModel) -> Tokenizer:
-    """
-    Instantiate the huggingface tokenizer.
-    :param model: the model doing the embedding
-    :return:
-    """
-    tokdir = Path(__file__).parent / "tokenizers"
-    if model == NomicTextEmbeddingModel.nomic_embed_text_v1_5:
-        tokenizer = Tokenizer.from_file(str(tokdir / "bert-base-uncased.json"))
-        # TODO: change to 8192
-        tokenizer.enable_truncation(max_length=2048)
-        tokenizer.enable_padding(pad_id=0, pad_token="[PAD]", pad_to_multiple_of=64)
-    else:
-        raise Exception(f"Could not determine tokenizer for model: {model.value}")
-    return tokenizer
-
-
-def create_sagemaker_request_for_batch(texts: List[str], tokenizer: Tokenizer):
-    """
-    Tokenizes and creates a Triton embedding request from a list of texts.
-
-    Args:
-        texts: List of texts to be batched.
-        tokenizer: Tokenizer to use.
-
-    Returns:
-        HTTP Request object for Triton server.
-    """
-    encoded = tokenizer.encode_batch(texts)
-
-    input_ids = np.array([e.ids for e in encoded], dtype=np.int32)
-    attention_mask = np.array([e.attention_mask for e in encoded], dtype=np.int32)
-
-    inputs = []
-    outputs = []
-
-    inputs.append(InferInput("input_ids", list(input_ids.shape), "INT32"))
-    inputs.append(InferInput("attention_mask", list(input_ids.shape), "INT32"))
-
-    # Initialize the data
-    inputs[0].set_data_from_numpy(input_ids, binary_data=True)
-    inputs[1].set_data_from_numpy(attention_mask, binary_data=True)
-
-    # have to set to binary since http doesn't natively support fp16
-    outputs.append(InferRequestedOutput("embedding", binary_data=True))
-    request, header_length = InferenceServerClient.generate_request_body(
-        inputs=inputs, outputs=outputs
-    )
-    return request, header_length
-
-
-def batch_sagemaker_requests(texts: List[str], batch_size=32):
-    """Yield sagemaker triton requests from specified size from list of texts.
-    One request will be yielded for each batch.
-    Batch size should be set based on GPU provisioned for sagemaker endpoint.
-
-    Args:
-        texts: List of texts to be batched.
-        batch_size: Size of each batch.
-
-    Yields:
-        Batches of data with size equal to batch_size.
-    """
-
-    tokenizer = load_tokenizer(NomicTextEmbeddingModel.nomic_embed_text_v1_5)
-    for i in range(0, len(texts), batch_size):
-        yield create_sagemaker_request_for_batch(texts[i : i + batch_size], tokenizer)
-
-
 def parse_sagemaker_response(response):
     """
     Parse response from a sagemaker triton server and return the embedding as a numpy array.
@@ -142,19 +60,11 @@ def parse_sagemaker_response(response):
         response: The response from the sagemaker triton server.
 
     Returns:
-        Numpy array of embeddings.
+        np.float16 array of embeddings.
     """
     # Parse json header size length from the response
-    header_length_prefix = (
-        "application/vnd.sagemaker-triton.binary+json;json-header-size="
-    )
-    header_length_str = response["ContentType"][len(header_length_prefix) :]
-
-    # Read response body
-    result = InferenceServerClient.parse_response_body(
-        response["Body"].read(), header_length=int(header_length_str)
-    )
-    return result.as_numpy("embedding")
+    resp = json.loads(response["Body"].read().decode())
+    return np.array(resp["embeddings"], dtype=np.float16)
 
 
 def embed_text(
@@ -180,15 +90,10 @@ def embed_text(
     embeddings = []
 
     embeddings = []
-    for embed_request, header_length in batch_sagemaker_requests(
-        texts, batch_size=batch_size
-    ):
+    for i in range(0, len(texts), batch_size):
+        batch = json.dumps({"texts": texts[i : i + batch_size]})
         response = client.invoke_endpoint(
-            EndpointName=sagemaker_endpoint,
-            Body=embed_request,
-            ContentType="application/vnd.sagemaker-triton.binary+json;json-header-size={}".format(
-                header_length
-            ),
+            EndpointName=sagemaker_endpoint, Body=batch, ContentType="application/json"
         )
         embeddings.append(parse_sagemaker_response(response))
     return np.vstack(embeddings)
