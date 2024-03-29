ef85d498aac8b9ce50ada5ff152d6cbef40af0a4
Wilson Marcilio-Jr <wilson_jr@outlook.com>
2024-03-29 16:02:09 -0300

    add explanatory comments

--- nomic/data_inference.py
+++ nomic/data_inference.py
@@ -69,8 +69,8 @@ class NomicProjectOptions(BaseModel):
         n_epochs: How many dataset passes to train the projection model. Default: `None` (Auto-inferred).
         model: Sets which model to perform projection. Possible values: `nomic-project-v1` or `nomic-project-v2`. Default: `nomic-project-v1`. 
         local_neighborhood_size: Only used when `model` is `nomic-project-v2`. Controls the size of the neighborhood used in one of the nomic-project-v2 steps. Min value: `max(n_neighbors, 1)`; max value: `128`. Default: `None` (Auto-inferred). 
-        spread: Determines how tight together points will be. It is recommended leaving this value as the default `None` (Auto-inferred).
-        rho: Only used when `model` is `nomic-project-v2`. Controls if points will be more scattered or tight inside clusters. Min value: `0`; max value: `1`. It is recommended leaving this value as the default `None` (Auto-inferred).
+        spread: Determines how tight together points will be, which bigger values meaning more spacement among points. Min value: `0`; max value `inf`. It is recommended leaving this value as the default `None` (Auto-inferred).
+        rho: Only used when `model` is `nomic-project-v2`. Controls if points will appear tight and in apparent blobs (when `rho` approaches to `1`). Min value: `0`; max value: `1`. It is recommended leaving this value as the default `None` (Auto-inferred).
     '''
 
     n_neighbors: Optional[int] = None
