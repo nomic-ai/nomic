8657e2c7cfeb5ecadacf2de021b1b1b0a09a8388
Wilson Marcilio-Jr <wilson_jr@outlook.com>
2024-03-29 13:11:52 -0300

    complete NomicProjectionOptions docs

--- nomic/data_inference.py
+++ nomic/data_inference.py
@@ -65,8 +65,12 @@ class NomicProjectOptions(BaseModel):
     Options for Nomic 2D Dimensionality Reduction Model
 
     Args:
-        n_neighbors: The number of neighbors to use when approximating the high dimensional embedding space during reduction.
-        n_epochs: How many dataset passes to train the projection model.
+        n_neighbors: The number of neighbors to use when approximating the high dimensional embedding space during reduction. Default: `None`.
+        n_epochs: How many dataset passes to train the projection model. Default: `None`.
+        model: Sets which model to perform projection. Possible values: `nomic-project-v1` or `nomic-project-v2`. Default: `nomic-project-v1`. 
+        local_neighborhood_size: Only used when `model` is `nomic-project-v2`. Controls size of the neighborhood used in one of the nomic-project-v2 steps. Min value: `max(n_neighbors, 1)`; max value: `128`. Default: `None`. 
+        spread: Determines how tight together points will be. It is recommended leaving this value as the default `None`.
+        rho: Only used when `model` is `nomic-project-v2`. Controls the visual layout of the projection with respect to cluster visualization. Min value: 0; max value: 1. It is recommended leaving this value as the default `None`.
     '''
 
     n_neighbors: Optional[int] = None
