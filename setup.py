6347be2a8c6e82bc19eb7081b769f78552d46e36
Jared Van Bortel <jared@nomic.ai>
2024-04-11 17:04:39 -0400

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description = 'The official Nomic python client.'
     
 setup(
     name='nomic',
-    version='3.0.22',
+    version='3.0.23',
     url='https://github.com/nomic-ai/nomic',
     description=description,
     long_description=description,
