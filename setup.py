ff9669ffc53768616a1680143a4df3bce4383a57
Richard Guo <richardg7890@gmail.com>
2024-06-04 16:49:43 -0400

    bump to 31

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ description = "The official Nomic python client."
 
 setup(
     name="nomic",
-    version="3.0.30",
+    version="3.0.31",
     url="https://github.com/nomic-ai/nomic",
     description=description,
     long_description=description,
