f95fb60023ec0ee8221ead52c5d7d3337a7e5f4b
Andriy Mulyar <andriy.mulyar@gmail.com>
2024-06-17 11:13:20 -0400

    Add backwords compatibility for what get's sent to the API

--- nomic/data_inference.py
+++ nomic/data_inference.py
@@ -88,9 +88,18 @@ class NomicTopicOptions(BaseModel):
     """
 
     build_topic_model: bool = True
-    topic_label_field: Optional[str] = Field(default=None, alias="community_description_target_field")
+    topic_label_field: Optional[str] = Field(default=None)
     cluster_method: str = "fast"
     enforce_topic_hierarchy: bool = False
+
+    def dict(self, **kwargs):
+        #TODO can be removed after Atlas Cloud v0.0.85 reaches all released tenants
+        # Get the original dictionary
+        original_dict = super().dict(**kwargs)
+        # Replace the key if topic_label_field exists in the original dictionary
+        if 'topic_label_field' in original_dict:
+            original_dict['community_description_target_field'] = original_dict.pop('topic_label_field')
+        return original_dict
 
 
 class NomicDuplicatesOptions(BaseModel):
