016b3e112f0bd990366324fa4f21719c7f7df8fa
Zach Nussbaum <zanussbaum@gmail.com>
2024-06-25 12:16:29 -0400

    fix: remove unused bytes

--- examples/image/map_cifar10.py
+++ examples/image/map_cifar10.py
@@ -1,9 +1,6 @@
 from nomic import embed
 from nomic import atlas
 from tqdm import tqdm
-import numpy as np
-import base64
-from io import BytesIO
 
 from datasets import load_dataset
 
@@ -28,10 +25,6 @@ max_embeddings = 100_000
 
 for idx, image in enumerate(tqdm(dataset)):
     images.append(image['img'])
-    buffered = BytesIO()
-    image['img'].save(buffered, format="PNG")
-
-    b64img = base64.b64encode(buffered.getvalue()).decode('utf-8')
     datums.append({'id': str(idx),
                    'label': labels[image['label']],
                   }
