1585e2d7c4e888fe70c26cc7c098c8caca224816
Richard Guo <richardg7890@gmail.com>
2024-06-27 17:08:57 -0400

    display dataset link instead

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ description = "The official Nomic python client."
 
 setup(
     name="nomic",
-    version="3.0.34",
+    version="3.0.35",
     url="https://github.com/nomic-ai/nomic",
     description=description,
     long_description=description,
