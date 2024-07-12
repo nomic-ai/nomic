cb3b8d66a1a9a62cdf244aef46c52029285aa887
Zach Nussbaum <zanussbaum@gmail.com>
2024-07-12 13:06:24 -0400

    refactor: restore example

--- examples/map_text.py
+++ examples/map_text.py
@@ -1,4 +1,4 @@
-from nomic import atlas, AtlasDataset
+from nomic import atlas
 import numpy as np
 from datasets import load_dataset
 
@@ -7,21 +7,12 @@ dataset = load_dataset('ag_news')['train']
 max_documents = 100000
 subset_idxs = np.random.choice(len(dataset), size=max_documents, replace=False).tolist()
 documents = [dataset[i] for i in subset_idxs]
-documents = [{'id': i, 'text': doc['text']} for i, doc in enumerate(documents)]
 
-dataset = AtlasDataset(identifier="nomic/test-ag-news", unique_id_field='id')
-
-for start in range(0, len(documents), 10000):
-    dataset.add_data(documents[start:start+10000])
-
-dataset.create_index(indexed_field='text')
-
-
-# project = atlas.map_data(data=documents,
-#                           indexed_field='text',
-#                           identifier='News 100k Example',
-#                           description='News 100k Example from the ag_news dataset hosted on huggingface.'
-#                           )
-# print(project.maps)
+project = atlas.map_data(data=documents,
+                          indexed_field='text',
+                          identifier='News 100k Example',
+                          description='News 100k Example from the ag_news dataset hosted on huggingface.'
+                          )
+print(project.maps)
 
 
