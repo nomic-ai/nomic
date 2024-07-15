9f7363ba81a358a48a57691a87bd4255ebad4148
Zach Nussbaum <zanussbaum@gmail.com>
2024-07-15 12:37:55 -0400

    fix: update docstring to be more clear

--- nomic/dataset.py
+++ nomic/dataset.py
@@ -1356,7 +1356,7 @@ class AtlasDataset(AtlasClass):
         Args:
             data: A pandas DataFrame, list of dictionaries, or pyarrow Table matching the dataset schema.
             embeddings: A numpy array of embeddings: each row corresponds to a row in the table. Use if you already have embeddings for your datapoints.
-            blobs: A list of image paths, bytes, or PIL Images. Use if you want to add images to your dataset.
+            blobs: A list of image paths, bytes, or PIL Images. Use if you want to create an AtlasDataset using image embeddings over your images.
             pbar: (Optional). A tqdm progress bar to update.
         """
         if embeddings is not None:
