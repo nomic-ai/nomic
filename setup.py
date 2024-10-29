de03cdc9c9ef5af745ded719b4b79777dfb04032
Benjamin Schmidt <bmschmidt@gmail.com>
2024-10-29 12:00:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ description = "The official Nomic python client."
 
 setup(
     name="nomic",
-    version="3.1.4",
+    version="3.1.3",
     url="https://github.com/nomic-ai/nomic",
     description=description,
     long_description=description,
