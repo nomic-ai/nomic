5e07b8b58d5e42d87d41e7161fcee7b4c1c06653
Zach Nussbaum <zanussbaum@gmail.com>
2024-10-29 21:50:06 -0400

    fix: do 50k point map

--- examples/map_multilingual.py
+++ examples/map_multilingual.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 
 dataset = load_dataset("bbaaaa/iwslt14-de-en", split="train")
 
-max_documents = 100000
+max_documents = 50_000
 selected = dataset[:max_documents]["translation"]
 
 documents = []
@@ -14,8 +14,8 @@ for doc in selected:
     documents.append(de_data)
 project = atlas.map_data(data=documents,
                           indexed_field='text',
-                          identifier='English-German 100k Translations',
-                          description='100k Examples from the iwslt14-de-en dataset hosted on huggingface.',
+                          identifier='English-German 50k Translations',
+                          description='50k Examples from the iwslt14-de-en dataset hosted on huggingface.',
                           embedding_model='gte-multilingual-base',
                           )
 print(project.maps)
