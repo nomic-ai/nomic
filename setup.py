062b78d82e241f873eef0462c409ef3f85972273
Zach Nussbaum <zanussbaum@gmail.com>
2025-01-22 17:50:25 -0500

    chore(main): release 3.4.1 (#374)
    adding deprecations to docstrings for delete_data, update_maps, update_indices methods of AtlasDataset (#370) (58e7a5c)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md") as f:
 
 setup(
     name="nomic",
-    version="3.4.0",
+    version="3.4.1",
     url="https://github.com/nomic-ai/nomic",
     description=description,
     long_description=long_description,
