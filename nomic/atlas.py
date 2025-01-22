2f860b474c977a64d02654dca53fae90a3624689
Max Cembalest <70534565+mcembalest@users.noreply.github.com>
2025-01-22 15:03:34 -0500

    fix: ValueError returned when calling map_data on blobs and embeddings (#373)
    fix: ValueError returned when calling map_data on blobs and embeddings

--- nomic/atlas.py
+++ nomic/atlas.py
@@ -63,6 +63,10 @@ def map_data(
         modality = "text"
 
     if blobs is not None:
+        if embeddings is not None:
+            raise ValueError(
+                "You cannot pass both `blobs` and `embeddings` to map_data(). To create a map of images, include `blobs` and not `embeddings`. To create a map of embeddings with images as metadata, include your images as a field in your `data` parameter."
+            )
         # change this when we support other modalities
         modality = "image"
         indexed_field = "_blob_hash"
