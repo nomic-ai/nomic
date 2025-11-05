9a3cd489ee9ba2df3f9511d75cf77232d75b219b
Jared Van Bortel <jared@nomic.ai>
2025-11-05 17:14:45 -0500

    chore(main): release 3.8.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md") as f:
 
 setup(
     name="nomic",
-    version="3.7.0",
+    version="3.8.0",
     url="https://github.com/nomic-ai/nomic",
     description=description,
     long_description=long_description,
